9f8c3b97ab4f328a57a79d072653a8d5ccfc39d6
hussienmousa128 <hussien.mousa128@gmail.com>
2025-12-09 20:22:37 +0300

    feat: add placeholder routes for jobs and login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { Landing } from './features/landing/landing/landing';
+import { Jobs } from './features/jobs/jobs/jobs';
+import { Login } from './features/auth/login/login';
 
 export const routes: Routes = [
   {path:'',component:Landing},
+  {path:'jobs',component:Jobs},
+  {path:'auth/login', component:Login},
+
   {path:'**',redirectTo:'',pathMatch:'full'}
 ];
---
+++ src/app/features/jobs/jobs/jobs.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jobs',
+  standalone:true,
+  imports: [],
+  templateUrl: './jobs.html',
+  styleUrl: './jobs.scss',
+})
+export class Jobs {
+
+}
